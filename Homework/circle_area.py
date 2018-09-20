838af6b710b4082bec1a8ffdce422dfc20011869
An <hanhan392003@gmail.com>
2018-09-20 19:22:33 +0700

    initial commit

--- Homework/circle_area.py
+++ Homework/circle_area.py
@@ -1,8 +1,24 @@
 import math
-r = float(input("Please enter the radius of the circle: "))
 
-pi = math.pi
-area = r*r*pi
-area_round = round(area, 2)
+loop = True
+while loop == True:
+    try:
+        r = float(input("Please enter the radius of the circle: "))
 
-print("The area of the circle:", area_round)
+        if r <= 0 :
+           pass
+        else:
+            pi = math.pi
+            area = r*r*pi
+            area_round = round(area, 2)
+
+            print("The area of the circle:", area_round)
+            
+            ask = input("Press E to exit or another key to continue ")
+            if ask == "e" or ask == "E":
+                loop = False
+            else:
+                pass
+        
+    except :
+       pass
--- Homework/temperature_convert.py
+++ Homework/temperature_convert.py
@@ -11,7 +11,7 @@ while loop == True :
             t_c = float(input("Please enter the temperature in Celcius: "))
             t_f = t_c * 1.8 + 32
             print(t_c,"(C) =", t_f, "(F)")
-            ask = input("Press E to exit or another key to continue")
+            ask = input("Press E to exit or another key to continue ")
             if ask == "e" or ask == "E":
                 loop = False
             else:
